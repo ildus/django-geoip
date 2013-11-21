225ce4d358850bd91831557f00e6931526e63b53
Ildus K <k-dus@yandex.ru>
2013-11-21 11:57:43 +0600

    country name in City

--- django_geoip/models.py
+++ django_geoip/models.py
@@ -49,9 +49,16 @@ class City(models.Model):
     """
     region = models.ForeignKey(Region, related_name='cities')
     name = models.CharField(_('city name'), max_length=255)
+    country_name = models.CharField(_("country_name"), blank=True, max_length=255)
     is_base = models.BooleanField(_('is base'), default=False)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.country_name:
+            self.country_name = self.region.country.name
+
+        super(City, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
--- django_geoip/urls.py
+++ django_geoip/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls.defaults import *
+from django.conf.urls import url, patterns
 from django_geoip.views import set_location
 
 urlpatterns = patterns('',
